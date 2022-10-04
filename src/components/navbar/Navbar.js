560521a0bfbe8facb78785073f60f2fd80f4593e
TanviralamSust <tanviralam997@gmail.com>
2022-10-04 00:14:28 -0600

    Added navbar styling and elements

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,7 +1,11 @@
+import "./navbar.scss"
 export default function Navbar() {
     return(
-        <>
-            Navbar
-        </>
+        <div className="nav-container">
+            <div className="search-container">
+                <input type="text" className="item-container" placeholder="Name or Term" />
+                <button className="item-container">Search</button>
+            </div>
+        </div>
     )
 }
